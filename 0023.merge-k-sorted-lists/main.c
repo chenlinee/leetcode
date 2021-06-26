d805759a7c3979d9c3cc357afd0a311dcfae567f
Chen Lin <ciichen235@gmail.com>
2021-06-26 21:18:57 +0800

    add 0023.merge k sorted lists

--- 0023.merge-k-sorted-lists/main.c
+++ 0023.merge-k-sorted-lists/main.c
@@ -1,8 +1,103 @@
 #include <stdio.h>
 #include "solution.h"
+#include "stdlib.h"
+#include <string.h>
+
+
+void init_Lists(int a[][10], struct ListNode *list_p[], int *listsSize)
+{
+    for(int row = 0; row < 10; ++row)
+    {
+        struct ListNode *p = NULL;
+        for(int col = 0; col < 10; ++col)
+        {
+            if(col == 0)
+            {
+                if(a[row][col] < 0)
+                {
+                    list_p[row] = NULL;
+                    *listsSize = row;
+                    return;
+                }
+                list_p[row] = (struct ListNode*)malloc(sizeof(struct ListNode));
+                p = list_p[row];
+                p->val = a[row][col];
+                p->next = NULL;
+                continue;
+            }
+            if(a[row][col] >= 0)
+            {
+                p->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+                p = p->next;
+                p->val = a[row][col];
+                p->next = NULL;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+
+void print_list(struct ListNode* list_p[])
+{
+    printf("[");
+    for(int i = 0; i < 10; ++i)
+    {
+        printf("[");
+        for(struct ListNode* p = list_p[i]; p != NULL; p = p->next)
+        {
+            printf("%d", p->val);
+            if(p->next != NULL)
+            {
+                printf(", ");
+            }
+        }
+        if(i == 9 || list_p[i + 1] == NULL)
+        {
+            printf("]");
+            break;
+        }
+        else
+        {
+            printf("], ");
+        }
+    }
+    printf("]\n");
+}
 
 int main(){
-    //convert();
+    int a[10][10] = {
+        {-1},
+        {-1},
+        {-1},
+        {-1}
+    };
+
+    struct ListNode* lists[10];
+    for(int i = 0; i < 10; ++i)
+    {
+        lists[i] = NULL;
+    }
+    int listsSize = 10;
+
+    //init_Lists(a, lists, &listsSize);
+    
+    print_list(lists);
+
+    struct ListNode *ret = mergeKLists(lists, listsSize);
+
+    printf("ret: [");
+    for(;ret != NULL; ret = ret->next)
+    {
+        printf("%d", ret->val);
+        if(ret->next != NULL)
+        {
+            printf(", ");
+        }
+    }
+    printf("]\n");
 
     return 0;
 }
--- 0023.merge-k-sorted-lists/solution.c
+++ 0023.merge-k-sorted-lists/solution.c
@@ -10,7 +10,7 @@
  * };
  */
 
-#define MAX_INT 0x7fffffff
+#define NULL_LIST -1
 
 typedef struct FTree_t {
     int* failure;
@@ -19,21 +19,80 @@ typedef struct FTree_t {
     int success;
 } *FTree;
 
+void FTree_Sort(FTree ftree)
+{
+    int *failure = ftree->failure;
+    struct ListNode** lists = ftree->lists;
+
+    int success_pos = ftree->success + ftree->size / 2;
+
+    printf("lists[ftree->success] = %p\n", lists[ftree->success]);
+    if(lists[ftree->success] == NULL)
+    {
+        failure[success_pos] = NULL_LIST;
+    }
+    for(int i = success_pos; i > 0; i = (i - 1) / 2)
+    {
+        // 父节点队列空，继续往上走
+        if(failure[(i - 1) / 2] == NULL_LIST)
+        {
+            continue;
+        }
+        // 自己队列空，让父节点往上走
+        if(failure[success_pos] == NULL_LIST)
+        {
+            ftree->success = failure[(i - 1) / 2];
+            success_pos = ftree->success + ftree->size / 2;
+            failure[(i - 1) / 2] = NULL_LIST;
+            continue;
+        }
+        // 两者都不为空，开始比较
+        int root = lists[failure[(i - 1) / 2]]->val;
+        int success = lists[ftree->success]->val;
+        if(root <= success)
+        {
+            int pre_success = ftree->success;
+            ftree->success = failure[(i - 1) / 2];
+            success_pos = ftree->success + ftree->size / 2;
+            failure[(i - 1) / 2] = pre_success;
+        }
+    }
+}
+
 void FTree_Init_Sort(FTree ftree)
 {
     int *failure = ftree->failure;
     struct ListNode** lists = ftree->lists;
     int success[ftree->size];
 
+    // copy leaf node to success tree
+    // check if any list is empty
     for(int i = ftree->size / 2; i < ftree->size; i++)
     {
         success[i] = failure[i];
+
     }
 
+    int left, right;
     for(int i = ftree->size - 1; i > 1; i = i - 2)
     {
-        int left = lists[success[i]]->val, right = lists[success[i-1]]->val;
-        if(left < right)
+        if(success[i-1] == NULL_LIST)
+        {
+            failure[i / 2 - 1] = success[i-1];
+            success[i / 2 - 1] = success[i];
+
+            continue;
+        }
+        if(success[i] == NULL_LIST) 
+        {
+            failure[i / 2 - 1] = success[i];
+            success[i / 2 - 1] = success[i - 1];
+            continue;
+        }
+        
+        left = lists[success[i]]->val;
+        right = lists[success[i-1]]->val;
+        if(left <= right)
         {
             failure[i / 2 - 1] = success[i-1];
             success[i / 2 - 1] = success[i];
@@ -43,6 +102,7 @@ void FTree_Init_Sort(FTree ftree)
             failure[i / 2 - 1] = success[i];
             success[i / 2 - 1] = success[i - 1];
         }
+
     }
 
     ftree->success = success[0];
@@ -54,21 +114,29 @@ void FTree_Init_Sort(FTree ftree)
 FTree FTree_Init(struct ListNode** lists, int listsSize)
 {
     int treeNodeNum = listsSize * 2 - 1;
-    int* failure = malloc(treeNodeNum * sizeof(int));
+    int* failure = (int *)malloc(treeNodeNum * sizeof(int));
 
-    // put List's first node to failure tree.
-    // the first non-leaf node sequence number should be listsSize - 1.
-    //   * first node sequence number of failure tree is 0, last is treeNodeNum - 1.
-    //              *0 (listsSize = 6)                *0 (listsSize = 5)
-    //          *1          *2                    *1       *2
-    //      *3      *4    *5   *6               *3   *4  *5   *6
-    //    *7  *8  *9  *a                     *7   *8
-    for(int i = treeNodeNum - 1; i > listsSize - 2; --i)
+    // * put List's first node to failure tree.
+    // * the first non-leaf node sequence number should be listsSize - 1.
+    //    * first node sequence number of failure tree is 0, last is treeNodeNum - 1.
+    //                *0 (listsSize = 6)                *0 (listsSize = 5)
+    //            *1          *2                    *1       *2
+    //        *3      *4    *5   *6               *3   *4  *5   *6
+    //      *7  *8  *9  *a                     *7   *8
+    // * check if any list is empty
+    for(int i = treeNodeNum / 2; i < treeNodeNum; ++i)
     {
-        failure[i] = treeNodeNum - i - 1;
+        if(lists[i - treeNodeNum / 2] != NULL)
+        {
+            failure[i] = i - treeNodeNum / 2;
+        }
+        else
+        {
+            failure[i] = NULL_LIST;
+        }
     }
 
-    FTree ftree = malloc(sizeof(struct FTree_t));
+    FTree ftree = (FTree)malloc(sizeof(struct FTree_t));
     ftree->failure = failure;
     ftree->size = treeNodeNum;
     ftree->lists = lists;
@@ -85,12 +153,38 @@ void FTree_Deinit(FTree ftree)
 }
 
 struct ListNode* mergeKLists(struct ListNode** lists, int listsSize){
-    if(listsSize < 2)
+    if(listsSize == 0)
+    {
+        return NULL;
+    }
+    if(listsSize == 1)
     {
         return *lists;
     }
     FTree ftree = FTree_Init(lists, listsSize);
 
-    
+    struct ListNode *ret = NULL, *current = NULL;
+    while(ftree->success != NULL_LIST && lists[ftree->success] != NULL)
+    {
+        struct ListNode *new_node = (struct ListNode*)malloc(sizeof(struct ListNode));
+        printf("success = %d\n", ftree->success);
+        new_node->val = lists[ftree->success]->val;
+        new_node->next = NULL;
+        if(ret == NULL)
+        {
+            ret = new_node;
+            current = new_node;
+        }
+        else
+        {
+            current->next = new_node;
+            current = current->next;
+        }
+
+        lists[ftree->success] = lists[ftree->success]->next;
+        FTree_Sort(ftree);
+    }
+
+    return ret;
 }
 
